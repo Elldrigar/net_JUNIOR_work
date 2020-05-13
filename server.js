82a06441240828685660608b309edff2b9710589
Artur Gawron <artur@gawron.me>
2020-05-14 00:02:08 +0200

    Define routes

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const connectDB = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+//Connect Database
 connectDB();
 
 app.get('/', (req, res) => res.send('API Running!'));
+
+//Define Routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profiles', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
