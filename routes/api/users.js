798f85a3b56f0c45a90775f7e68f9a93ef008716
Artur Gawron <artur@gawron.me>
2020-05-15 15:51:53 +0200

    Add express validator check ( name, email & password)

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 // @route   POST api/users
 // @desc    Register user
 // @Access  Public
-router.post('/', (req, res) => {
-	console.log(req.body);
+router.post(
+	'/',
+	[
+	 check('name', 'Imię jest wymagane')
+		 .not()
+		 .isEmpty(),
+	 check('email', 'Podaj poprawny email')
+		 .isEmail(),
+	 check('password', 'Podaj Hasło min. 8 znaków')
+		 .isLength({ min:8 })
+],
+	(req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
 	res.send('User route');
 });
 
