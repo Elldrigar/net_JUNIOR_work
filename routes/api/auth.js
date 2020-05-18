7ecc5acce7ed34e7c9bf51f455cec8e0af95d8db
Artur Gawron <artur@gawron.me>
2020-05-18 17:17:29 +0200

    Finish Registration/Login Users

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const { check, validationResult } = require('express-validator/check');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 // @route   GET api/auth
 // @desc    Test route
@@ -15,5 +19,59 @@ router.get('/', auth, async (req, res) => {
 		res.status(500).send('Server Error!')
 	}
 });
+
+// @route   POST api/auth
+// @desc    Authenticate user and get token
+// @Access  Public
+router.post(
+	'/',
+	[
+		check('email', 'Podaj poprawny email')
+			.isEmail(),
+		check('password', 'Hasło jest wymagane')
+			.exists()
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		const { email, password } = req.body;
+		try {
+			let user = await User.findOne( { email });
+
+			if (!user) {
+				return res
+					.status(400)
+					.json({ errors: [ {msg: 'Nieprawidłowe dane uwierzytelniające'}] });
+			}
+			const isMatch = await bcrypt.compare(password, user.password);
+
+			if (!isMatch) {
+				return res
+					.status(400)
+					.json({ errors: [ {msg: 'Nieprawidłowe dane uwierzytelniające'}] });
+			}
+
+			const payload = {
+				user: {
+					id: user.id
+				}
+			}
+			
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{ expiresIn: 360000 }, // TO DO Change to 3600,
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+		} catch(err) {
+			console.error(err.message);
+			res.status(500).send('Server Error!')
+		}
+	});
 
 module.exports = router;
