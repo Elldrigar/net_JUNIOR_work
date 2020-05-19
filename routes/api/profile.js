cd87101d3a748478a867daf3a2f631538ff7b611
Artur Gawron <artur@gawron.me>
2020-05-19 13:57:14 +0200

    Add route Get profile by user ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -117,4 +117,18 @@ router.get('/', async (req, res) => {
 	}
 });
 
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @Access  Public
+router.get('/user/:user_id', async (req, res) => {
+	try {
+		const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+
+		if (!profile) return res.status(400).json({ msg: 'Nie znaleziono profilu użytkownika'});
+		res.json(profile);
+	} catch(err) {
+		console.error(err.message);
+		res.status(500).send('Server Error!')
+	}
+});
 module.exports = router;
