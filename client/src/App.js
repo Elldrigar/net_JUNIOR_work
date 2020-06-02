66b957c4dc91f5e04c75af6b3b0a4c93b56f3476
Artur Gawron <artur@gawron.me>
2020-06-02 15:45:35 +0200

    Import Landing i Navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react';
+import Navbar from "./components/layout/navbar";
+import Landing from "./components/layout/landing";
 import './App.css';
 
 const App = () => (
     <Fragment>
-      <h1>App Works!</h1>
+      <Navbar />
+      <Landing />
     </Fragment>
 );
 
