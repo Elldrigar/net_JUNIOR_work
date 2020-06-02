54814466d21e766521b4cbc9f7fad01d4ef4549b
Artur Gawron <artur@gawron.me>
2020-06-02 19:13:20 +0200

    Format code with Prettier 🧙🏻‍♂️

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/navbar';
 import Landing from './components/layout/landing';
 import Login from './components/auth/Login';
@@ -7,19 +7,18 @@ import Register from './components/auth/Register';
 import './App.css';
 
 const App = () => (
-    <Router>
-        <Fragment>
-            <Navbar />
-            <Route exact path='/' component={Landing} />
-            <section className='container'>
-                <Switch>
-                    <Route exact path='/register' component={Register} />
-                    <Route exact path='/login' component={Login} />
-                </Switch>
-            </section>
-        </Fragment>
-    </Router>
-
+   <Router>
+      <Fragment>
+         <Navbar />
+         <Route exact path="/" component={Landing} />
+         <section className="container">
+            <Switch>
+               <Route exact path="/register" component={Register} />
+               <Route exact path="/login" component={Login} />
+            </Switch>
+         </section>
+      </Fragment>
+   </Router>
 );
 
 export default App;
