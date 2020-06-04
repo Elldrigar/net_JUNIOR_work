468eb0b916ce171292fd214ce733a3d17ff66a6a
Artur Gawron <artur@gawron.me>
2020-06-04 15:36:04 +0200

    Redirect to dashboard after registration success

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,9 +3,9 @@ import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert';
 import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
-const Register = ({ setAlert, register }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
    const [formData, setFormData] = useState({
       name: '',
       email: '',
@@ -24,6 +24,11 @@ const Register = ({ setAlert, register }) => {
          register({ name, email, password });
       }
    };
+
+   //*** REDIRECT IF REGISTRATED ***//
+   if (isAuthenticated) {
+      return <Redirect to='/dashboard' />
+   }
 
    return (
       <Fragment>
@@ -107,9 +112,14 @@ const Register = ({ setAlert, register }) => {
 Register.propTypes = {
    setAlert: PropTypes.func.isRequired,
    register: PropTypes.func.isRequired,
+   isAuthenticated: PropTypes.bool,
 };
 
+const mapStateToProps = state => ({
+   isAuthenticated: state.auth.isAuthenticated
+});
+
 export default connect(
-    null,
+    mapStateToProps,
     { setAlert, register }
     )(Register);
