c2a807f3815b3a1911908a1218396dbb3ba2876a
Artur Gawron <artur@gawron.me>
2020-06-06 00:26:32 +0200

    Add links to Edit profile,add exp & and edu

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -1,11 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 const DashboardActions = () => {
-	return (
-		<div>
-
-		</div>
-	)
-}
+   return (
+      <div className="dash-buttons">
+         <Link to="/edit-profile" className="btn">
+            <i className="fas fa-user-circle" /> Edytuj profil
+         </Link>
+         <Link to="/add-experience" className="btn">
+            <i className="fas fa-laptop-code" /> Dodaj doświadczenie
+         </Link>
+         <Link to="/add-education" className="btn text-primary">
+            <i className="fas fa-graduation-cap" /> Dodaj wykrztałcenie
+         </Link>
+      </div>
+   );
+};
 
 export default DashboardActions;
