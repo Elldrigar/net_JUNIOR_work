4c1c6d716e4627983dca590ba4c35d86850fba72
Artur Gawron <artur@gawron.me>
2020-06-07 22:24:57 +0200

    Add experience in profile Action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_PROFILE, PROFILE_ERROR } from './types';
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from './types';
 
 //*** GET CURRENT USER PROFILE***//
 export const getCurrentProfile = () => async (dispatch) => {
@@ -21,7 +21,7 @@ export const getCurrentProfile = () => async (dispatch) => {
 
 //*** CREATE OR UPDATE PROFILE***//
 export const createProfile = (formData, history, edit = false) => async (
-   dispatch
+   dispatch,
 ) => {
    try {
       const config = {
@@ -34,10 +34,43 @@ export const createProfile = (formData, history, edit = false) => async (
          type: GET_PROFILE,
          payload: res.data,
       });
-      dispatch(setAlert(edit ? 'Profil zaktualizowany' : 'Profil Utworzony', 'success'));
+      dispatch(
+         setAlert(
+            edit ? 'Profil zaktualizowany' : 'Profil Utworzony',
+            'success',
+         ),
+      );
       if (!edit) {
          history.push('/dashboard');
       }
+   } catch (err) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+         errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+      }
+      dispatch({
+         type: PROFILE_ERROR,
+         payload: { msg: err.response.statusText, status: err.response.status },
+      });
+   }
+};
+
+//*** ADD EXPERIENCE ***//
+export const addExperience = (formData, history) => async (dispatch) => {
+   try {
+      const config = {
+         headers: {
+            'Content-Type': 'application/json',
+         },
+      };
+      const res = await axios.put('/api/profile/experience', formData, config);
+      dispatch({
+         type: UPDATE_PROFILE,
+         payload: res.data,
+      });
+      dispatch(setAlert('Doświadczenie dodane', 'success'));
+      history.push('/dashboard');
    } catch (err) {
       const errors = err.response.data.errors;
 
