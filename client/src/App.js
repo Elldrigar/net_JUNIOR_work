512540b3e26532b6804cc4617db0d46acfa39621
Artur Gawron <artur@gawron.me>
2020-06-07 22:02:22 +0200

    Import Edit profile route

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Register from './components/auth/Register';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
 import Alert from './components/layout/Alert';
 import { Provider } from 'react-redux';
 import store from './store';
@@ -42,6 +43,11 @@ const App = () => {
                         exact
                         path="/create-profile"
                         component={CreateProfile}
+                     />
+                     <PrivateRoute
+                         exact
+                         path="/edit-profile"
+                         component={EditProfile}
                      />
                   </Switch>
                </section>
