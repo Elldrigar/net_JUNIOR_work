798f2146a5be96d242bd5bb117216f3c9dec2600
Artur Gawron <artur@gawron.me>
2020-06-09 01:27:25 +0200

    Add List Education component

---
+++ client/src/components/dashboard/Education.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+import { connect } from 'react-redux';
+
+const Experience = ({ experience }) => {
+   return <Fragment></Fragment>;
+};
+
+Experience.propTypes = {};
+
+export default connect()(Experience);
