00a73271744ee7090221290addd53a258db56b8f
Artur Gawron <artur@gawron.me>
2020-06-08 23:45:34 +0200

    Import addExperience as PrivateRoute

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
 import Alert from './components/layout/Alert';
 import { Provider } from 'react-redux';
 import store from './store';
@@ -48,6 +49,11 @@ const App = () => {
                          exact
                          path="/edit-profile"
                          component={EditProfile}
+                     />
+                     <PrivateRoute
+                         exact
+                         path="/add-experience"
+                         component={AddExperience}
                      />
                   </Switch>
                </section>
