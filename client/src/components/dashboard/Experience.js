5f18f723f70eca025a34182e7c09214eb7f0879a
Artur Gawron <artur@gawron.me>
2020-06-09 15:11:14 +0200

    Display experience from DB

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -4,7 +4,7 @@ import Moment from 'react-moment';
 import { connect } from 'react-redux';
 
 const Experience = ({ experience }) => {
-   const experiences = experience.map(exp => (
+   const experiences = experience.map((exp) => (
       <tr key={exp._id}>
          <td className="col1">{exp.company}</td>
          <td className="hide-sm col2">{exp.title}</td>
@@ -18,30 +18,33 @@ const Experience = ({ experience }) => {
          </td>
          <td>
             <button className="btn btn-danger">
-               <i className="far fa-trash-alt"/> Usuń
+               <i className="far fa-trash-alt" /> Usuń
             </button>
          </td>
       </tr>
    ));
 
-   return <Fragment>
-	   <h2 className="margin-vertical-2">Doświadczenie Zawodowe</h2>
-	   <table className="table">
-		   <thead>
-		   <tr>
-			   <th>Firma</th>
-			   <th className="hide-sm">Stanowisko</th>
-			   <th className="hide-sm">Lata</th>
-			   <th className="col4"/>
-		   </tr>
-		   </thead>
-		   <tbody>{experiences}</tbody>
-	   </table>
-   </Fragment>
+   return (
+      <Fragment>
+         <h2 className="margin-vertical-2">Doświadczenie Zawodowe</h2>
+         <table className="table">
+            <thead>
+               <tr>
+                  <th>Firma</th>
+                  <th className="hide-sm">Stanowisko</th>
+                  <th className="hide-sm">Lata</th>
+                  <th className="col4" />
+               </tr>
+            </thead>
+            <tbody>{experiences}</tbody>
+         </table>
+      </Fragment>
+   );
 };
 
 Experience.propTypes = {
-	experience: PropTypes.array.isRequired,
+   experience: PropTypes.array.isRequired,
 };
 
 export default Experience;
+g
