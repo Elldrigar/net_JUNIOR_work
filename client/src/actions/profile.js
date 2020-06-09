38baafe092a991be2c453535d9e6622095da3199
Artur Gawron <artur@gawron.me>
2020-06-09 15:19:18 +0200

    Add Delete Experience profile action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -111,3 +111,20 @@ export const addEducation = (formData, history) => async (dispatch) => {
       });
    }
 };
+
+//*** DELETE EXPERIENCE ***//
+export const deleteExperience = (id) => async (dispatch) => {
+   try {
+      const res = await axios.delete(`/api/profile/experience/${id}`);
+      dispatch({
+         type: UPDATE_PROFILE,
+         payload: res.data,
+      });
+      dispatch(setAlert('Doświadczenie usunięto', 'success'));
+   } catch (err) {
+      dispatch({
+         type: PROFILE_ERROR,
+         payload: { msg: err.response.statusText, status: err.response.status },
+      });
+   }
+};
