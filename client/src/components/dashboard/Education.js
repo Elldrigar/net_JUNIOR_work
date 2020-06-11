ca44db1259b0940452e945fe187583cb3954ac18
Artur Gawron <artur@gawron.me>
2020-06-11 13:06:13 +0200

    Add delete educationc action

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -2,46 +2,53 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
+import { deleteEducation } from '../../actions/profile';
 
-const Education = ({ education }) => {
-   const educations = education.map(edu => (
-       <tr key={edu._id}>
-          <td className="col1">{edu.school}</td>
-          <td className="hide-sm col2">{edu.degree}</td>
-          <td className="col3">
-             <Moment format="YYYY/MM/DD">{edu.from}</Moment> -{' '}
-             {edu.to === null ? (
-                 ' Trwa'
-             ) : (
-                 <Moment format="YYYY/MM/DD">{edu.to}</Moment>
-             )}
-          </td>
-          <td>
-             <button className="btn btn-danger">
-                <i className="far fa-trash-alt"/> Usuń
-             </button>
-          </td>
-       </tr>
+const Education = ({ education, deleteEducation }) => {
+   const educations = education.map((edu) => (
+      <tr key={edu._id}>
+         <td className="col1">{edu.school}</td>
+         <td className="hide-sm col2">{edu.degree}</td>
+         <td className="col3">
+            <Moment format="YYYY/MM/DD">{edu.from}</Moment> -{' '}
+            {edu.to === null ? (
+               ' Trwa'
+            ) : (
+               <Moment format="YYYY/MM/DD">{edu.to}</Moment>
+            )}
+         </td>
+         <td>
+            <button
+               onClick={() => deleteEducation(edu._id)}
+               className="btn btn-danger"
+            >
+               <i className="far fa-trash-alt" /> Usuń
+            </button>
+         </td>
+      </tr>
    ));
 
-   return <Fragment>
-      <h2 className="margin-vertical-2">Edukacja</h2>
-      <table className="table">
-         <thead>
-         <tr>
-            <th>Szkoła/Kurs</th>
-            <th className="hide-sm">Stopień</th>
-            <th className="hide-sm">Lata</th>
-            <th className="col4"/>
-         </tr>
-         </thead>
-         <tbody>{educations}</tbody>
-      </table>
-   </Fragment>
+   return (
+      <Fragment>
+         <h2 className="margin-vertical-2">Edukacja</h2>
+         <table className="table">
+            <thead>
+               <tr>
+                  <th>Szkoła/Kurs</th>
+                  <th className="hide-sm">Stopień</th>
+                  <th className="hide-sm">Lata</th>
+                  <th className="col4" />
+               </tr>
+            </thead>
+            <tbody>{educations}</tbody>
+         </table>
+      </Fragment>
+   );
 };
 
 Education.propTypes = {
    education: PropTypes.array.isRequired,
+    deleteEducation: PropTypes.func.isRequired,
 };
 
-export default Education;
+export default connect(null, { deleteEducation })(Education);
