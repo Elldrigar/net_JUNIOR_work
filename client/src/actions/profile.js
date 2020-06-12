b567fe263a2a5de0c6ef42b1f2f8a07a4ffe4bf2
Artur Gawron <artur@gawron.me>
2020-06-12 13:45:34 +0200

    Add GET GITHUB REPOS action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,6 +3,7 @@ import { setAlert } from './alert';
 import {
    GET_PROFILE,
    GET_PROFILES,
+   GET_REPOS,
    PROFILE_ERROR,
    UPDATE_PROFILE,
    CLEAR_PROFILE,
@@ -53,6 +54,23 @@ export const getProfileById = (userId) => async (dispatch) => {
 
       dispatch({
          type: GET_PROFILE,
+         payload: res.data,
+      });
+   } catch (err) {
+      dispatch({
+         type: PROFILE_ERROR,
+         payload: { msg: err.response.statusText, status: err.response.status },
+      });
+   }
+};
+
+// *** GET GITHUB REPOS *** //
+export const getProfileById = (username) => async (dispatch) => {
+   try {
+      const res = await axios.get(`/api/profile/github/${username}`);
+
+      dispatch({
+         type: GET_REPOS,
          payload: res.data,
       });
    } catch (err) {
