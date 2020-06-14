7cafcca8b13d02662de3770234b5bd90c1bbda6d
Artur Gawron <artur@gawron.me>
2020-06-14 22:45:42 +0200

    Add Edit profile button when logged in

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -24,6 +24,13 @@ const Profile = ({
                <Link to="/profiles" className="btn btn-light">
                   Wróć do wszystkich profili
                </Link>
+               {auth.isAuthenticated &&
+                  auth.loading === false &&
+                  auth.user._id === profile.user._id && (
+                     <Link to="/edit-profile" className="btn btn-dark">
+                        Edytuj Profil
+                     </Link>
+                  )}
             </Fragment>
          )}
       </Fragment>
