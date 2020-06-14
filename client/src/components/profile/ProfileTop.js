a869cd4a7b41bd65858d5406d86cb17cae76a0cc
Artur Gawron <artur@gawron.me>
2020-06-14 23:22:37 +0200

    Add ProfileTop pattern

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -1,16 +1,61 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 
-const ProfileTop = props => {
-	return (
-		<div>
-
-		</div>
-	)
-}
+const ProfileTop = ({
+   profile: {
+      status,
+      company,
+      location,
+      website,
+      social,
+      user: { name, avatar },
+   },
+}) => {
+   return (
+      <div className="profile-top bg-primary padding-2">
+         <img
+            className="img--round-bottom margin-vertical-1"
+            src={avatar}
+            alt="Gravatar"
+         />
+         <h1 className="large">{name}</h1>
+         <p className="lead">
+            {status} {company && <span> w {company}</span>}
+         </p>
+         <p>{location && <span>{location}</span>}</p>
+         <div className="icons margin-vertical-1">
+            {website && (
+               <a href={website}>
+                  <i className="fas fa-globe fa-2x" />
+               </a>
+            )}
+            {social && social.twitter && (
+               <a href={social.twitter}>
+                  <i className="fab fa-twitter fa-2x" />
+               </a>
+            )}
+            {social && social.facebook && (
+               <a href={social.facebook}>
+                  <i className="fab fa-facebook fa-2x" />
+               </a>
+            )}
+            {social && social.linkedin && (
+               <a href={social.linkedin}>
+                  <i className="fab fa-linkedin fa-2x" />
+               </a>
+            )}
+            {social && social.instagram && (
+               <a href={social.instagram}>
+                  <i className="fab fa-instagram fa-2x" />
+               </a>
+            )}
+         </div>
+      </div>
+   );
+};
 
 ProfileTop.propTypes = {
+   profile: PropTypes.object.isRequired,
+};
 
-}
-
-export default ProfileTop
+export default ProfileTop;
