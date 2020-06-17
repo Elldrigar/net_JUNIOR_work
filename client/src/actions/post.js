ef2d390dff98de8e75f202a0ba51bbeeb21208fb
Artur Gawron <artur@gawron.me>
2020-06-17 17:45:52 +0200

    Add get a single post Action

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import {ADD_POST, DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES} from './types';
+import {ADD_POST, DELETE_POST, GET_POSTS, GET_POST, POST_ERROR, UPDATE_LIKES} from './types';
 
 // *** GET POSTS *** //
 export const getPosts = () => async (dispatch) => {
@@ -87,6 +87,23 @@ export const addPost = (formData) => async (dispatch) => {
          payload: res.data,
       });
       dispatch(setAlert('Post został utworzony', 'success'));
+   } catch (err) {
+      dispatch({
+         type: POST_ERROR,
+         payload: { msg: err.response.statusText, status: err.response.status },
+      });
+   }
+};
+
+// *** GET POST *** //
+export const getPost = (id) => async (dispatch) => {
+   try {
+      const res = await axios.get(`/api/posts/${id}`);
+
+      dispatch({
+         type: GET_POST,
+         payload: res.data,
+      });
    } catch (err) {
       dispatch({
          type: POST_ERROR,
