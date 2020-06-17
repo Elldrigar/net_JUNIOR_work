cb405099f64aafbadd40d220e14ebeb213ec39e1
Artur Gawron <artur@gawron.me>
2020-06-17 14:45:00 +0200

    Add like post action

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR } from './types';
+import {GET_POSTS, POST_ERROR, UPDATE_LIKES} from './types';
 
 // *** GET POSTS *** //
 export const getPosts = () => async (dispatch) => {
@@ -17,4 +17,21 @@ export const getPosts = () => async (dispatch) => {
          payload: { msg: err.response.statusText, status: err.response.status },
       });
    }
+};
+
+// *** ADD LIKE *** //
+export const addLike = (postId) => async (dispatch) => {
+   try {
+      const res = await axios.put(`/api/posts/like/${postId}`);
+
+      dispatch({
+         type: UPDATE_LIKES,
+         payload: { id, likes: res.data },
+      });
+   } catch (err) {
+      dispatch({
+         type: POST_ERROR,
+         payload: { msg: err.response.statusText, status: err.response.status },
+      });
+   }g
 };
