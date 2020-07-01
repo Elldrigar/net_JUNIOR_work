f26c5f5025ea84e687b6c519dcc57afa1c44812e
Artur Gawron <artur@gawron.me>
2020-07-01 23:21:47 +0200

    Refactor and clean code

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -11,67 +11,70 @@ const bcrypt = require('bcryptjs');
 // @desc    Test route
 // @Access  Public
 router.get('/', auth, async (req, res) => {
-	try {
-		const user = await User.findById(req.user.id).select('-password');
-		res.json(user);
-	} catch (err) {
-		console.error(err.message);
-		res.status(500).send('Server Error!')
-	}
+   try {
+      const user = await User.findById(req.user.id).select('-password');
+      res.json(user);
+   } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error!');
+   }
 });
 
 // @route   POST api/auth
 // @desc    Authenticate user and get token
 // @Access  Public
 router.post(
-	'/',
-	[
-		check('email', 'Podaj poprawny email')
-			.isEmail(),
-		check('password', 'Hasło jest wymagane')
-			.exists()
-	],
-	async (req, res) => {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(400).json({ errors: errors.array() });
-		}
-		const { email, password } = req.body;
-		try {
-			let user = await User.findOne( { email });
+   '/',
+   [
+      check('email', 'Podaj poprawny email').isEmail(),
+      check('password', 'Hasło jest wymagane').exists(),
+   ],
+   async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+         return res.status(400).json({ errors: errors.array() });
+      }
+      const { email, password } = req.body;
+      try {
+         let user = await User.findOne({ email });
 
-			if (!user) {
-				return res
-					.status(400)
-					.json({ errors: [ {msg: 'Nieprawidłowe dane uwierzytelniające'}] });
-			}
-			const isMatch = await bcrypt.compare(password, user.password);
+         if (!user) {
+            return res
+               .status(400)
+               .json({
+                  errors: [{ msg: 'Nieprawidłowe dane uwierzytelniające' }],
+               });
+         }
+         const isMatch = await bcrypt.compare(password, user.password);
 
-			if (!isMatch) {
-				return res
-					.status(400)
-					.json({ errors: [ {msg: 'Nieprawidłowe dane uwierzytelniające'}] });
-			}
+         if (!isMatch) {
+            return res
+               .status(400)
+               .json({
+                  errors: [{ msg: 'Nieprawidłowe dane uwierzytelniające' }],
+               });
+         }
 
-			const payload = {
-				user: {
-					id: user.id
-				}
-			}
+         const payload = {
+            user: {
+               id: user.id,
+            },
+         };
 
-			jwt.sign(
-				payload,
-				config.get('jwtSecret'),
-				{ expiresIn: 360000 }, // TO DO Change to 3600,
-				(err, token) => {
-					if (err) throw err;
-					res.json({ token });
-				}
-			);
-		} catch(err) {
-			console.error(err.message);
-			res.status(500).send('Server Error!')
-		}
-	});
+         jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn: '7d' },
+            (err, token) => {
+               if (err) throw err;
+               res.json({ token });
+            },
+         );
+      } catch (err) {
+         console.error(err.message);
+         res.status(500).send('Server Error!');
+      }
+   },
+);
 
 module.exports = router;
